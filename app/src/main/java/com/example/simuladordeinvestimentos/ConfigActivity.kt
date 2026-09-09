66fc49e96cb78f7ede64bf7ed32c93144245653d
Elieudo Silva <elieudosilva95@gmail.com>
2026-09-08 22:21:24 -0300

    feat: adiciona navegação da configuração para resultado

--- app/src/main/java/com/example/simuladordeinvestimentos/ConfigActivity.kt
+++ app/src/main/java/com/example/simuladordeinvestimentos/ConfigActivity.kt
@@ -1,20 +1,29 @@
 package com.example.simuladordeinvestimentos
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ConfigActivity : AppCompatActivity() {
+
+    private lateinit var buttonCalcular: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_config)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        buttonCalcular = findViewById(R.id.btnCalcularInvestimento)
+
+        buttonCalcular.setOnClickListener {
+
+            val intent = Intent(this, ResultActivity::class.java)
+
+            startActivity(intent)
         }
     }
 }
